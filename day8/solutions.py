567b1e05680b06c146cbdd73ad9379d4b4ba5bad
Anna <annadahacker@gmail.com>
2022-12-14 17:38:16 +0100

    day 8, 2/2 pass

--- day8/solutions.py
+++ day8/solutions.py
@@ -1,9 +1,4 @@
-# Part 1: how many trees are visible from outside the grid?
-# def visible: 
-#   - a tree that is on one of the outer rows
-#   - a tree that is visible from at least one side: 
-#       that is (strictly) higher than all trees on at least 
-#       one side of it (inc. top and bottom)
+import math
 
 def main():
     grid = []
@@ -16,11 +11,55 @@ def main():
     print("Part 1:", count_visible(grid))
 
     # What is the highest scenic score possible for any tree?
-    # TODO
+    print("Part 2:", count_scenic_score(grid))
 
+# A tree's scenic score is found by multiplying together 
+# its viewing distance in each of the four directions.
+def count_scenic_score(grid):
+    size = len(grid)
+    max_score = 1
+
+    for x in range(1, size-1):
+        for y in range(1, size-1):
+            tree = grid[x][y]
+
+            top = [grid[i][y] for i in range(x)]
+            bottom = [grid[i][y] for i in range(x+1, size)]
+            left = [grid[x][i] for i in range(y)]
+            right = [grid[x][i] for i in range(y+1, size)]
+
+            # reverse to get same distance to tree
+            top.reverse()
+            left.reverse()
+            
+            max_score = max(max_score, scenic_score(tree, left, right, top, bottom))
+
+    return max_score
+
+def scenic_score(tree, left, right, top, bottom):
+    directions = [left, right, top, bottom]
+    direction_scores = []
+
+    for direction in directions:
+        direction_scores.append(visible_trees(tree, direction))
+
+    return math.prod(direction_scores)
+
+def visible_trees(tree, direction):
+    seen_trees = 0
+    for seen_tree in direction:
+        seen_trees += 1
+        if seen_tree >= tree:
+            return seen_trees
+    return seen_trees
+
+# def visible: 
+#   - a tree that is on one of the outer rows
+#   - a tree that is visible from at least one side: 
+#       that is (strictly) higher than all trees on at least 
+#       one side of it (inc. top and bottom)
 def count_visible(grid):
     size = len(grid)
-    print(size)
     sum = 2*size + (size-2)*2
 
     for x in range(1, size-1):
