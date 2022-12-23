016d956c095378c00d751c3edd5843da1204aae2
Anna <annadahacker@gmail.com>
2022-12-23 15:09:20 +0100

    day 11, part 1 PASS

--- day11/solutions.py
+++ day11/solutions.py
@@ -1,9 +1,49 @@
 from collections import defaultdict
+from heapq import nlargest
 
 def main():
 
-    monkeys = parse_monkeys('ex-input.txt')
-    print(monkeys)
+    monkeys = parse_monkeys('input.txt')
+
+    monkeys, inspected_items = stuff_slinging_shenanigans(20, monkeys)
+
+    print(monkey_business(inspected_items))
+
+
+def stuff_slinging_shenanigans(rounds, monkeys):
+    inspected_items = defaultdict(int)
+
+    for _ in range(rounds):
+        for monkey in monkeys:
+            for item in monkeys[monkey]['items']:
+                worry_level = operation(item, monkeys[monkey]['operator'], monkeys[monkey]['number'])
+                worry_level = int(worry_level/3)
+                
+                if worry_level % monkeys[monkey]['divider'] == 0:
+                    throw_to = monkeys[monkey]['true']
+                else:
+                    throw_to = monkeys[monkey]['false']
+                monkeys[throw_to]['items'].append(worry_level)
+
+                inspected_items[monkey] += 1
+
+            monkeys[monkey]['items'] = []
+
+    return monkeys, inspected_items
+
+def monkey_business(inspected_items):
+    most_active = nlargest(2, list(inspected_items.values()))
+    return most_active[0]*most_active[1]
+
+def operation(worry_level, operator, number):
+    match operator:
+        case '+':
+            return int(worry_level + number)
+        case '*':
+            return int(worry_level * number)
+        case '**':
+            return int(worry_level ** 2)
+    return -1
 
 def parse_monkeys(filename):
 
